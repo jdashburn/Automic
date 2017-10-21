0498f79e6710a5054a510471d21027fdb951ea9f
jdashburn <jdashburn@outlook.com>
2017-10-21 19:42:48 +1100

    Creation
    Initial Creation

---
+++ Automic/src/HelloAutomic.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author jdash
+ *
+ */
+public class HelloAutomic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Automic!");
+	}
+
+}
